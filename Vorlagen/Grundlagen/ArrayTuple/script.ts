06576c65065096d9fbc75c32241bcc61b7d73ce9
Danny <ydh.postkasten@yahoo.com>
2024-02-14 13:11:01 +0100

    DHE add file of tuple

--- Vorlagen/Grundlagen/ArrayTuple/script.ts
+++ Vorlagen/Grundlagen/ArrayTuple/script.ts
@@ -1,1 +1,13 @@
+let array = [ 1 , 2 , 3];
+console.log(array);
+
 console.log('Hier steht Type-Script');
+
+
+function printArray(array:number[]){
+    console.log(array);
+}
+
+printArray(array);
+printArray([26565]);
+printArray([78537]);
---
+++ Vorlagen/Grundlagen/Tuple/script.ts
@@ -0,0 +1,13 @@
+
+
+
+let tuple:[ number , number , string] = [27,34,'Sandra'];
+
+console.log('Hier steht Type-Script');
+
+function printTuple(tuple:[number,number, string]){
+    console.log(tuple);
+}
+
+printTuple(tuple);
+
