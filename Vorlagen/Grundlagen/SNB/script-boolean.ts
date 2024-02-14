4fb377a0acc1f25120f01c8803c74dc80006ba4e
Danny <ydh.postkasten@yahoo.com>
2024-02-14 12:52:50 +0100

    DHE add files

---
+++ Vorlagen/Grundlagen/SNB/script-boolean.ts
@@ -0,0 +1,17 @@
+  const arg  = 2;
+//const arg1 = 2;
+//const arg2 = 2;
+
+//let result = add(arg1,arg2);
+
+
+function check(arg:number):boolean{
+    if ( arg > 2){
+
+        return false;
+    }
+    return true;
+}
+
+console.log(result);
+
---
+++ Vorlagen/Grundlagen/SNB/script-number.ts
@@ -0,0 +1,12 @@
+const arg1 = 2;
+const arg2 = 2;
+
+let result = add(arg1,arg2);
+
+
+function add(arg1: number, arg2: number):number{
+    return arg1 + arg2;
+}
+
+console.log(result);
+
