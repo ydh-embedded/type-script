8723794a44aa8b867c46cf63530119e5387e453d
Danny <ydh.postkasten@yahoo.com>
2024-02-14 14:59:27 +0100

    DHE dev TS

--- Vorlagen/Grundlagen/TypeAlias/script.ts
+++ Vorlagen/Grundlagen/TypeAlias/script.ts
@@ -6,12 +6,13 @@ let result_1  =   add(arg_01,arg_11) ;
 
 
 
+type Add_able = number|string ;
 
 
 
 
-
-function add(argument1:string|number,argument2:string|number){
+//  function add(argument1:string|number,argument2:string|number){
+    function add(argument1:Add_able,argument2:Add_able){
 
     if( typeof (argument1) == 'number' && typeof (argument2) == 'number'){
         return argument1 + argument2 ;
@@ -20,5 +21,18 @@ function add(argument1:string|number,argument2:string|number){
         return argument1 + argument2 ;
     }
     return;
-}
+    }
 
+
+
+
+
+
+    function printer(arg:Add_able){
+        console.log(arg);
+    }
+
+
+    printer('Meine Ausgabe');
+
+    printer(28 + 14);
