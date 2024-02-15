53ef0350304cd4a955ffce694b94ef836f8586b8
Danny <ydh.postkasten@yahoo.com>
2024-02-15 09:53:19 +0100

    DHE add files ENUMs

---
+++ Vorlagen/Grundlagen/ENUMs/script.ts
@@ -0,0 +1,19 @@
+
+
+
+console.log('Ab Hier steht Type-Script');
+
+const n0: number = 0 ;
+
+    if(!n0){
+        console.log(false);
+    } else {
+        console.log(true);
+    }
+const stringLeer = '' ;
+
+    if(!stringLeer){
+        console.log(false);
+    } else {
+        console.log(true);
+    }
