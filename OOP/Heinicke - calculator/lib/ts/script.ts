2a05aed3624b3f95f7185dcf60c5119271bba7c5
Danny <ydh.postkasten@yahoo.com>
2024-02-16 08:10:51 +0100

    dev calculator

---
+++ OOP/Heinicke - calculator/lib/ts/script.ts
@@ -0,0 +1,17 @@
+
+
+import * as lib from "./lib.js";
+
+    const P01 = new lib.Point2D(0,0);
+
+        console.log(P01);
+    
+    const P02 = new lib.Point3D(0,0,0);
+        console.log(P02);
+    
+        
+
+
+
+
+
